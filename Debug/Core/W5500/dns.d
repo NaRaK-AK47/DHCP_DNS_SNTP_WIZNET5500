669571897edfcf8b1cef468e30f258f633f25bf4
Karan <karansanghvi98@gmail.com>
2024-05-09 18:29:14 +0530

    First Commit

---
+++ Debug/Core/W5500/dns.d
@@ -0,0 +1,9 @@
+Core/W5500/dns.o: ../Core/W5500/dns.c ../Core/W5500/socket.h \
+ ../Core/W5500/wizchip_conf.h ../Core/W5500/W5500/w5500.h \
+ C:/Embedded-C/Personal_course/Stm32_Excercise/DHCP_DNS_SNTP_STM32C6T6_WIZ5500/Core/W5500/wizchip_conf.h \
+ ../Core/W5500/dns.h
+../Core/W5500/socket.h:
+../Core/W5500/wizchip_conf.h:
+../Core/W5500/W5500/w5500.h:
+C:/Embedded-C/Personal_course/Stm32_Excercise/DHCP_DNS_SNTP_STM32C6T6_WIZ5500/Core/W5500/wizchip_conf.h:
+../Core/W5500/dns.h:
